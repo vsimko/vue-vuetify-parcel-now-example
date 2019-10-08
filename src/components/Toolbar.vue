337fbcb4c03e281e82925ffd1aaf23add62dcd99
Viliam Simko <viliam.simko@gmail.com>
2019-10-09 00:37:39 +0200

    feat: add Toolbar

---
+++ src/components/Toolbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app-bar color="blue-grey darken-1" app dark>
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+    <v-toolbar-title>{{ message }}</v-toolbar-title>
+
+    <div class="flex-grow-1"></div>
+
+    <v-btn icon>
+      <v-icon>mdi-heart</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+      <v-icon>mdi-magnify</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      message: "Using Parcel In A Vue.js App"
+    };
+  }
+};
+</script>
